74f9f278bbf0e9658830d88778f918f76d05103f
Logikaaa <123231827+Logikaaa@users.noreply.github.com>
2025-02-22 15:05:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWid
 
 import json
 
+notes = {
+    "Ілля Сидорчук": {
+        "текст": "Хороший хлопчик"
+    }
+}
+
+with open("notes_data.json", "w") as file:
+    json.dump(notes, file)
+
 app = QApplication([])
 
 notes_win = QWidget()
@@ -41,6 +50,50 @@ col_2.addLayout(row_4)
 layout_notes.addLayout(col_1, stretch = 2)
 layout_notes.addLayout(col_2, stretch = 1)
 notes_win.setLayout(layout_notes)
+
+def add_note():
+    note_mane, ok = QInputDialog.getText(notes_win, "Додати замітку", "Назва замітки:")
+    if ok and note_mane != '':
+        notes[note_mane] = {"текст": ""}
+        list_notes.addItem(note_mane)
+        print(notes)
+
+def show_note():
+    key = list_notes.selectedItems()[0].text()
+    print(key)
+    field_text.setText(notes[key]["текст"])
+
+def save_note():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        notes[key]["текст"] = field_text.toPlainText()
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes,file, sort_keys = True, ensure_ascii = False)
+        print(notes)
+    else:
+        print('Замітка для збереження не вибрана!')
+
+def note_del():
+    if list_notes.selectedItems():
+        key = list_notes.selectedItems()[0].text()
+        del notes[key]
+        list_notes.clear()
+        field_text.clear()
+        list_notes.addItems(notes)
+        with open('notes_data.json', 'w') as file:
+            json.dump(notes,file, sort_keys = True, ensure_ascii = False)
+        print(notes)
+    else:
+        print('Замітка для вилучення не обрана!')
+
+button_note_create.clicked.connect(add_note)
+list_notes.itemClicked.connect(show_note)
+button_note_save.clicked.connect(save_note)
+button_note_del.clicked.connect(note_del)
+
+with open('notes_data.json', 'r') as file:
+    notes = json.load(file)
+list_notes.addItems(notes)
 
 notes_win.show()
 app.exec_()
